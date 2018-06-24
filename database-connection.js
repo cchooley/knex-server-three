a54e1dd93b61571a6f59501d14d04d33c1e427da
Conor Hooley <cchooley@gmail.com>
2018-06-24 10:44:41 -0600

    edited database connection, retrying deploy

--- database-connection.js
+++ database-connection.js
@@ -1,3 +1,4 @@
+/*
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./server/knexfile')
 const environmentConfig = config[environment]
@@ -5,3 +6,7 @@ const knex = require('knex')
 const connection = knex(environmentConfig)
 
 module.exports = connection
+*/
+
+const CONFIG = require("./knexfile")[process.env.NODE_ENV || "development"];
+module.exports = require("knex")(CONFIG);
